816bec63c77c211eff3d595b626c9c48bdff0696
ogh826 <eys555@naver.com>
2024-10-09 20:15:07 +0900

    [Feat] Fox 스폰 구현

--- Assets/FoxController.cs
+++ Assets/FoxController.cs
@@ -9,7 +9,6 @@ public class FoxController : MonoBehaviour
     [SerializeField] ARRaycastManager raycastManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
     [SerializeField] Rigidbody rigid;
-    [SerializeField] Vector2 center;
     [SerializeField] float speed;
     [SerializeField] Camera cam;
 
@@ -24,16 +23,23 @@ public class FoxController : MonoBehaviour
 
     private void Update()
     {
+        // 터치가 없을 경우 넘어감
         if(Input.touchCount == 0) return;
-
+        // 터치가 1개 이상 있을 경우
         if(Input.touchCount > 0)
         {
             if(raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.Planes))
             {
+                // 순간이동
                 //transform.position = hits[0].pose.position;
+
+                // 이동하고자 하는 방향을 구하기
                 Vector3 dir = hits[0].pose.position - transform.position;
+                // 땅에 붙어있도록 y값은 0으로 설정
                 dir.y = 0;
+                // rigidbody를 이용한 움직임(이후에 FixedUpdate로 전환해야 할 듯)
                 rigid.MovePosition(transform.position + dir * Time.deltaTime * speed);
+                // 회전값을 카메라를 쳐다보도록 변경 (이후에 이동할때는 그쪽 방향을 쳐다보도록 bool과 코루틴을 활용해야 할 듯)
                 transform.rotation = Quaternion.LookRotation(cam.transform.position - transform.position);
             }
         }
--- Assets/FoxSpawn.cs
+++ Assets/FoxSpawn.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class FoxSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] ARRaycastManager raycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    [SerializeField] GameObject fox;
+    [SerializeField] Vector2 screenCenter;
+    bool isSpawn;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        raycastManager = GetComponent<ARRaycastManager>();
+        isSpawn = false;
+    }
+    private void Update()
+    {
+        screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+        if (!isSpawn && raycastManager.Raycast(screenCenter, hits, TrackableType.Planes))
+        {
+            fox.SetActive(true);
+            isSpawn = true;
+            hits = null;
+        }
     }
 }
